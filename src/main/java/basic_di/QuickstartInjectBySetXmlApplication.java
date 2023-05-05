2d16d5b4431bfab358c53f2cf4b3c95117ab0944
zhang.jinhe <zhang.jinhe@sunlord.com.cn>
2023-05-05 13:54:23 +0800

    set注入，关联bean赋值

---
+++ src/main/java/basic_di/QuickstartInjectBySetXmlApplication.java
@@ -0,0 +1,17 @@
+package basic_di;
+
+import basic_di.bean.Cat;
+import basic_di.bean.Person;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class QuickstartInjectBySetXmlApplication {
+    public static void main(String[] args) {
+        BeanFactory beanFactory=new ClassPathXmlApplicationContext("basic_di/inject-set.xml");
+        Person person = beanFactory.getBean(Person.class);
+        System.out.println(person);
+
+        Cat cat = beanFactory.getBean(Cat.class);
+        System.out.println(cat);
+    }
+}
---
+++ src/main/java/basic_di/bean/Cat.java
@@ -0,0 +1,30 @@
+package basic_di.bean;
+
+public class Cat {
+    private String name;
+    private Person master;
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", master=" + master +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person getMaster() {
+        return master;
+    }
+
+    public void setMaster(Person master) {
+        this.master = master;
+    }
+}
