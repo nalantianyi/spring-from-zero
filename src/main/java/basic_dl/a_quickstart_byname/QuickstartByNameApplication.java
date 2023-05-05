fbf01c7047bd07a7d782d449e5b10e7a00f460d1
zhang.jinhe <zhang.jinhe@sunlord.com.cn>
2023-05-05 13:29:11 +0800

    根据class类型注入

--- src/main/java/basic_dl/a_quickstart_byname/QuickstartByNameApplication.java
+++ src/main/java/basic_dl/a_quickstart_byname/QuickstartByNameApplication.java
@@ -7,8 +7,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class QuickstartByNameApplication {
     public static void main(String[] args) {
-        BeanFactory factory = new ClassPathXmlApplicationContext("basic_dl/quickstart-bytype.xml");
-        Person person = factory.getBean(Person.class);
-        System.out.println(person);
+        BeanFactory factory1 = new ClassPathXmlApplicationContext("basic_dl/quickstart-byname.xml");
+        Person person1 = (Person) factory1.getBean("person");
+        BeanFactory factory2 = new ClassPathXmlApplicationContext("basic_dl/quickstart-bytype.xml");
+        Person person2 = factory2.getBean(Person.class);
+        System.out.println(person1);
+        System.out.println(person2);
+
     }
 }
