9fea8d93bf6e4488cc06b946226cb9874bd917d1
zhang.jinhe <zhang.jinhe@sunlord.com.cn>
2023-05-05 13:50:49 +0800

    set注入，关联bean赋值

---
+++ src/main/java/basic_di/bean/Person.java
@@ -0,0 +1,30 @@
+package basic_di.bean;
+
+public class Person {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
